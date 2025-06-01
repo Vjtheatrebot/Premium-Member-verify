e713bb8e76001a0149497008dbc86c89867a5c23
Vjtheatrebot <vjtheatrerips@gmail.com>
2025-06-01 23:14:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "your_bot_token_here"
-ADMIN_USER_ID = 123456789  # Your Telegram user ID
+BOT_TOKEN = "7882462319:AAFgIumwqaqhVMqbSfPsMmXrHdklY5T5cJA"
+ADMIN_USER_ID = 1490611763  # Your Telegram user ID
 CHANNEL_INVITE_LINK = "https://t.me/+your_private_channel_link"
